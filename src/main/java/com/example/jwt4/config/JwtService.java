11b49156378547b87d79700bf4998d0be2533b28
baekseungchan1117 <bsc7386@gmail.com>
2023-11-17 13:46:24 +0900

    token create & Validation

---
+++ src/main/java/com/example/jwt4/config/JwtService.java
@@ -0,0 +1,45 @@
+package com.example.jwt4.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${application.security.jwt.secret-key}")
+    private String secretkey;
+    @Value("${application.security.jwt.expiration}")
+    private String jwtExpiraion;
+
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignKey(){
+        byte[] KeyBytes = Decoders.BASE64.decode(secretkey);
+        return Keys.hmacShaKeyFor(KeyBytes);
+    }
+
+
+}
