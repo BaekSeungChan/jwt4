4fdf5704475e4bced966b8f82b53d60dacaa3a39
baekseungchan1117 <bsc7386@gmail.com>
2023-11-17 13:58:35 +0900

    토큰 검증

--- src/main/java/com/example/jwt4/config/JwtService.java
+++ src/main/java/com/example/jwt4/config/JwtService.java
@@ -64,6 +64,19 @@ public class JwtService {
                 .compact();
     }
 
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()));
+    }
+
+    private boolean isTokenValid(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     public Claims extractAllClaims(String token){
         return Jwts
                 .parserBuilder()
